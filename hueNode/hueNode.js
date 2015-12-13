066335513c1d92e86faf693304d2e90a43a6d04b
Arnd Brugman <arndbrugman@gmail.com>
2015-12-13 11:40:17 +0100

    Added support for BridgeCache

--- hueNode/hueNode.js
+++ hueNode/hueNode.js
@@ -1,45 +1,46 @@
 var huepi = require('../huepi');
 
 var MyHue = new huepi();
-MyHue.Username = '085efe879ee3ed83c04efc28a0da03d3';
+// OBSOLETE! May not be set externally, comes from bridge when whitelisting! MyHue.Username = '085efe879ee3ed83c04efc28a0da03d3';
 var HeartbeatInterval;
 
 ConnectMyHue();
 
 function ConnectMyHue() {
-
-  console.log('Bridge IP: Trying to Discover hue Bridge via hue Portal');
-  MyHue.PortalDiscoverLocalBridges().then(function GetBridgeConfig() {
-    MyHue.BridgeGetData().then(function EnsureWhitelisting() {
-      console.log('hue Bridge IP: ' + MyHue.BridgeIP);
-      if (!MyHue.BridgeUsernameWhitelisted) {
-        console.log('Please press connect button on the hue Bridge');
-        MyHue.BridgeCreateUser().then(function ReReadBridgeConfiguration() {
-          return ConnectMyHue();
-        }, function UnableToCreateUseronBridge() {
-          console.log('Unable to Create User on hue Bridge');
-        });
-      } else {
+  console.log('Discovering hue Bridge via hue Portal');
+  MyHue.PortalDiscoverLocalBridges().then(function BridgesDiscovered() {
+    console.log('hue Bridge IP: ' + MyHue.BridgeIP);
+    MyHue.BridgeGetConfig().then(function BridgeConfigReceived() {
+      console.log('hue Bridge Name: ' + MyHue.BridgeName);
+      MyHue.BridgeGetData().then(function BridgeDataReceived() {
         StartHeartbeat();
-      }
+      }, function UnableToRetreiveBridgeData() {
+        console.log('Please press connect button on the hue Bridge');
+        MyHue.BridgeCreateUser().then(function BridegeUserCreated() {
+          console.log('Connected');
+          StartHeartbeat();
+        }, function UnableToCreateUseronBridge() {
+          console.log('.Please press connect button on the hue Bridge.');
+          setTimeout(ConnectMyHue(), 1000);
+        });
+      });
     }, function UnableToRetreiveBridgeConfiguration() {
       console.log('Unable to Retreive hue Bridge Configuration');
-      return ConnectMyHue();
+      setTimeout(ConnectMyHue(), 1000);
     });
   }, function UnableToDiscoverLocalBridgesViaPortal() {
     console.log('Unable to find Local hue Bridge via hue Portal');
-    return ConnectMyHue();
+    setTimeout(ConnectMyHue(), 1000);
   });
 }
 
 function StartHeartbeat() {
-
-  console.log('hue Bridge Name: ' + MyHue.BridgeName);
   console.log('Found hue Bridge and Whitelisted');
   //MyHue.GroupOn(0);
   //MyHue.GroupEffectNone(0);
-  HeartbeatInterval = setInterval(StatusHeartbeat, 1000);
-  StatusHeartbeat(); // Execute Immediate Too!
+  MyHue.GroupAlertSelect(0);
+  HeartbeatInterval = setInterval(StatusHeartbeat, 2500);
+  //StatusHeartbeat(); // Execute Immediate Too!
 }
 
 function StatusHeartbeat() {
